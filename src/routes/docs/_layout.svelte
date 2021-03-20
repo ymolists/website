64d94f87e3b1633c300707ac05f33870873e602f
Mike Nikles <mikenikles@gmail.com>
2021-03-20 22:59:58 +0000

    Add a docs layout & a docs menu placeholder.

---
+++ src/routes/docs/_layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Menu from "../../components/docs/menu.svelte";
+</script>
+
+<h1>Docs</h1>
+
+<div class="flex">
+  <div>
+    <Menu />
+  </div>
+  <div>
+    <slot />
+  </div>
+</div>
