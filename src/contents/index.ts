ba147c784aa912d3127ffb1f765e51968ebee490
Mike Nikles <mikenikles@gmail.com>
2021-03-20 12:41:54 +0000

    Format all files with Prettier.

--- src/contents/index.ts
+++ src/contents/index.ts
@@ -1,17 +1,18 @@
-import type { Feature } from '../types/feature.type';
+import type { Feature } from "../types/feature.type";
 
 export const features: Feature[] = [
-    {
-        title: 'Introducing Pre-builds',
-        paragraph: 'We invented prebuilds so application code, configuration and infrastructure can all be stored as machine-executable code in your git repositories and applied to dev environments automatically and continuously.',
-        moreButton: {
-            text: 'More on prebuilds',
-            href: '/docs/prebuilds/'
-        },
-        documentationLink: null,
-        preview: {
-            name: 'terminal.png',
-            alt: 'A terminal that displays Gitpod prebuild output.'
-        }
-    }
-]
+  {
+    title: "Introducing Pre-builds",
+    paragraph:
+      "We invented prebuilds so application code, configuration and infrastructure can all be stored as machine-executable code in your git repositories and applied to dev environments automatically and continuously.",
+    moreButton: {
+      text: "More on prebuilds",
+      href: "/docs/prebuilds/",
+    },
+    documentationLink: null,
+    preview: {
+      name: "terminal.png",
+      alt: "A terminal that displays Gitpod prebuild output.",
+    },
+  },
+];
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import autoProcess from 'svelte-preprocess';
-import { mdsvex } from 'mdsvex';
+import autoProcess from "svelte-preprocess";
+import { mdsvex } from "mdsvex";
 
 const mode = process.env.NODE_ENV;
-const dev = mode === 'development';
+const dev = mode === "development";
 
 export const preprocess = {
   sourceMap: dev,
   ...autoProcess({ postcss: true, scss: true }),
-  ...mdsvex()
+  ...mdsvex(),
 };
