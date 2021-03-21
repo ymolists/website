5862958ed8d4b156c9649ec8ef408e971a8b2a82
Mike Nikles <mikenikles@gmail.com>
2021-03-21 16:35:09 +0000

    Accommodate for SSR & client-side routing.

--- src/components/docs/search.svelte
+++ src/components/docs/search.svelte
@@ -1,15 +1,26 @@
-<script lang="ts">
-  const docSearchJSVersion = "2.6.3";
+<script>
+  import { onMount } from "svelte";
 
-  const docSearchAction = (node: HTMLElement) => {
-    (window as any).docsearch({
-      apiKey: "1a880f3060e9ff81ff84087fc90878fc",
-      indexName: "gitpod",
-      inputSelector: `#${node.getAttribute("id")}`,
-      // Set debug to true to inspect the dropdown
-      debug: false,
-    });
+  const docSearchJSVersion = "2.6.3";
+  const docSearchInputSelector = "search-doc-input";
+
+  let docSearchInput;
+
+  const initDocSearch = () => {
+    if (docSearchInput) {
+      window.docsearch({
+        apiKey: "1a880f3060e9ff81ff84087fc90878fc",
+        indexName: "gitpod",
+        inputSelector: `#${docSearchInputSelector}`,
+        // Set debug to true to inspect the dropdown
+        debug: false,
+      });
+    }
   };
+
+  onMount(() => {
+    initDocSearch();
+  });
 </script>
 
 <svelte:head>
@@ -18,12 +29,13 @@
     href="https://cdn.jsdelivr.net/npm/docsearch.js@{docSearchJSVersion}/dist/cdn/docsearch.min.css"
   />
   <script
+    on:load={initDocSearch}
     src="https://cdn.jsdelivr.net/npm/docsearch.js@{docSearchJSVersion}/dist/cdn/docsearch.min.js"></script>
 </svelte:head>
 
 <input
-  use:docSearchAction
+  bind:this={docSearchInput}
   type="search"
   placeholder="Search"
-  id="search-doc-input"
+  id={docSearchInputSelector}
 />
