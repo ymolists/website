37d5da530a871ff30c73ea13a9dd1d41203b9d74
Mike Nikles <mikenikles@gmail.com>
2021-03-21 02:22:15 +0000

    Add remark-copy-linked-files to dynamically copy assets referenced in Markdown files.

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import autoProcess from "svelte-preprocess";
 import { mdsvex } from "mdsvex";
+import remarkCopyLinkedFiles from "remark-copy-linked-files";
 
 const mode = process.env.NODE_ENV;
 const dev = mode === "development";
@@ -7,5 +8,16 @@ const dev = mode === "development";
 export const preprocess = {
   sourceMap: dev,
   ...autoProcess({ postcss: true, scss: true }),
-  ...mdsvex({ extensions: [".md"] }),
+  ...mdsvex({
+    extensions: [".md"],
+    remarkPlugins: [
+      [
+        remarkCopyLinkedFiles,
+        {
+          destinationDir: "static/images/docs",
+          staticPath: "/images/docs",
+        },
+      ],
+    ],
+  }),
 };
