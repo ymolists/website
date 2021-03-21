b79ecb5db2ca77314b0d726eb1c5ed73779a6e58
Mike Nikles <mikenikles@gmail.com>
2021-03-21 04:41:29 +0000

    Add docs/release-notes

--- src/utils/remark-docs-set-image-path.js
+++ src/utils/remark-docs-set-image-path.js
@@ -1,11 +1,12 @@
 import visit from "unist-util-visit";
 
+const imagesRelativeUrlPattern = "../static/images/";
+
 const visitor = (node) => {
-  if (
-    node.type === "image" &&
-    node.url.startsWith("../../../static/images/docs/")
-  ) {
-    node.url = node.url.substring("../../../static".length);
+  if (node.type === "image" && node.url.indexOf("../static/images/") > 0) {
+    node.url = node.url.substring(
+      node.url.indexOf(imagesRelativeUrlPattern) + "../static".length
+    );
   }
 };
 
