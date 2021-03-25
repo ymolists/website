d36615e54504cb132362f999e08e2123f78d3ea8
Mike Nikles <mikenikles@gmail.com>
2021-03-25 02:00:03 +0000

    Manually format .ts files as a one-off.

--- src/types/project.type.ts
+++ src/types/project.type.ts
@@ -1,9 +1,8 @@
 export type Project = {
-    logo: string;
-    title: string;
-    githubUrl?: string;
-    gitlabUrl?: string;
-    bitbucketUrl?: string
-    alt: string;
-  };
-  
+  logo: string;
+  title: string;
+  githubUrl?: string;
+  gitlabUrl?: string;
+  bitbucketUrl?: string;
+  alt: string;
+};
