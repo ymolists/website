cb5c2a3caf6b32aa5c32a8cded427a033d8a5714
Mike Nikles <mikenikles@gmail.com>
2021-03-26 17:07:22 +0000

    Apply the same Markdown styling approach as we use for docs.

--- src/components/blog/blog-content-layout.svelte
+++ src/components/blog/blog-content-layout.svelte
@@ -16,54 +16,8 @@
   });
 </script>
 
-<style>
-  .content :global(blockquote) {
-    @apply my-14 p-8 rounded-lg bg-orange-700;
-  }
-
-  .content :global(blockquote code) {
-    @apply p-2 rounded-lg bg-orange-900;
-  }
-
-  .content :global(code) {
-    @apply p-2 rounded-lg bg-orange-700;
-  }
-
-  .content :global(pre) {
-    @apply p-8 rounded-lg bg-gray-300 overflow-x-scroll;
-  }
-
-  .content :global(pre code) {
-    @apply bg-transparent;
-  }
-
-  .content :global(h1) {
-    @apply text-h1;
-  }
-
-  .content :global(h2) {
-    @apply text-h2;
-  }
-
-  .content :global(h3) {
-    @apply text-h3;
-  }
-
-  .content :global(h4) {
-    @apply text-h4;
-  }
-
-  .content :global(h5) {
-    @apply text-h5;
-  }
-
-  .content :global(h6) {
-    @apply text-h6;
-  }
-
-  .content :global(p) {
-    @apply my-8;
-  }
+<style lang="scss" global>
+  @import "../../assets/blog";
 </style>
 
 <img
@@ -77,6 +31,6 @@
   </p>
 </div>
 <div>TODO: Share icons</div>
-<div class="pt-8 content">
+<div class="pt-8 content-blog">
   <slot />
 </div>
