32d92fbcf1e4d984e085fc12eeda61b2cf166bb8
Mike Nikles <mikenikles@gmail.com>
2021-03-28 18:03:05 +0000

    Add Open Graph tags.

---
+++ src/components/open-graph.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { stores } from "@sapper/app";
+  import type { OpenGraph, Type } from "../types/open-graph.type";
+
+  export let data: OpenGraph;
+
+  const {
+    description = "Gitpod streamlines developer workflows by providing prebuilt, collaborative development environments in your browser - powered by VS Code.",
+    image = "https://www.gitpod.io/images/media-image.jpg",
+    title = "Gitpod - Dev environments built for the cloud",
+    type = "website",
+  } = data || {};
+
+  const { page } = stores();
+  const url = `https://${$page.host}${$page.path}`;
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta
+    name="keywords"
+    content="dev environment, development environment, devops, cloud ide, github ide, gitlab ide, javascript, online ide, web ide, code review"
+  />
+  <meta name="description" content={description} />
+
+  <!-- https://ogp.me -->
+  <meta property="og:image" content={image} />
+  <meta property="og:description" content={description} />
+  <meta property="og:title" content={title} />
+  <meta property="og:type" content={type.toString()} />
+  <meta property="og:url" content={url} />
+</svelte:head>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,8 +1,10 @@
 <script>
   import Footer from "../components/footer.svelte";
   import Nav from "../components/main-nav/index.svelte";
+  import OpenGraph from "../components/open-graph.svelte";
 </script>
 
+<OpenGraph />
 <Nav />
 <main>
   <slot />
