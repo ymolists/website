9e89720de83704a95dda2c96372982b489b9fe99
Mike Nikles <mikenikles@gmail.com>
2021-03-28 20:29:27 +0000

    Add Google Analytics.

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,8 +1,10 @@
 <script>
+  import GoogleAnalytics from "../components/google-analytics.svelte";
   import Footer from "../components/footer.svelte";
   import Nav from "../components/main-nav/index.svelte";
 </script>
 
+<GoogleAnalytics />
 <Nav />
 <main>
   <slot />
