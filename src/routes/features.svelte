d14f2543fcaf4a7c0b3fef7ac8247f4fcfa6deff
Mike Nikles <mikenikles@gmail.com>
2021-03-30 21:57:56 +0000

    Add the first three sreencast previews to the Features page.

--- src/routes/features.svelte
+++ src/routes/features.svelte
@@ -1,53 +1,55 @@
-
-<style lang="scss">
-.halfimages article,
-.cards {
-  display: flex;
-  grid-gap: 2rem;
-  flex-flow: row wrap;
-  margin-bottom: 2rem;
-  justify-content: center;
-}
-.halfimages article > div {
-  flex: 0 1 calc( 50% - 2rem );
-  min-width: 400px;
-  align-self: center;
-}
-.halfimages article p {
-  margin-bottom: 2rem;
-}
-.cards.double > div {
-  flex: 0 1 calc( 50% - 2rem );
-  min-width: 400px;
-  align-self: stretch;
-}
-.cards.triple > div {
-  flex: 0 1 calc( 33% - 2rem );
-  min-width: 300px;
-  align-self: stretch;
-  text-align: center;
-}
-.triple h3 {
-  min-height: 3rem;
-}
-.triple .card {
-  display: grid;
-  grid-template-columns: 100%;
-  grid-template-rows: 33% 3rem 1fr 1.5rem;
-}
-.triple a {
-  color: var(--brand-ripe);
-}
-section h3:first-child,
-section h3:first-child + p {
-  text-align: center;
-}
-</style>
-
-
 <script>
   import OpenGraph from "../components/open-graph.svelte";
+  import ScreencastPreview from "../components/screencasts/preview.svelte";
+  import screencasts from "../contents/screencasts";
+
+  const firstThreeScreencasts = screencasts.slice(0, 3);
 </script>
+
+<style lang="scss">
+  .halfimages article,
+  .cards {
+    display: flex;
+    grid-gap: 2rem;
+    flex-flow: row wrap;
+    margin-bottom: 2rem;
+    justify-content: center;
+  }
+  .halfimages article > div {
+    flex: 0 1 calc(50% - 2rem);
+    min-width: 400px;
+    align-self: center;
+  }
+  .halfimages article p {
+    margin-bottom: 2rem;
+  }
+  .cards.double > div {
+    flex: 0 1 calc(50% - 2rem);
+    min-width: 400px;
+    align-self: stretch;
+  }
+  .cards.triple > div {
+    flex: 0 1 calc(33% - 2rem);
+    min-width: 300px;
+    align-self: stretch;
+    text-align: center;
+  }
+  .triple h3 {
+    min-height: 3rem;
+  }
+  .triple .card {
+    display: grid;
+    grid-template-columns: 100%;
+    grid-template-rows: 33% 3rem 1fr 1.5rem;
+  }
+  .triple a {
+    color: var(--brand-ripe);
+  }
+  section h3:first-child,
+  section h3:first-child + p {
+    text-align: center;
+  }
+</style>
 
 <OpenGraph
   data={{
@@ -58,50 +60,53 @@ section h3:first-child + p {
   }}
 />
 <header>
-<h1 class="h2">Features</h1>
-<p>
-  Gitpod will always have a free-to-use option. For those looking for a little
-  more we have a few powerful solutions.
-</p>
+  <h1 class="h2">Features</h1>
+  <p>
+    Gitpod will always have a free-to-use option. For those looking for a little
+    more we have a few powerful solutions.
+  </p>
 </header>
 
 <section class="halfimages">
-<article>
-  <div>
-    <h2 class="h3">Save Time with Prebuilt Workspaces</h2>
-    <p class="text-large">
-      Gitpod continuously builds your git branches like a CI server. This means
-      no more waiting for dependencies to be downloaded and builds to finish.
-    </p>
-    <p>
-      <a href="/docs/prebuilds/" class="btn-secondary">More about Prebuilt Workspaces</a>
-    </p>
-  </div>
-  <div>
-    <!-- TODO: Insert the terminal output animation from the homepage -->
-    <img src="//placekitten.com/400/400" alt="" role="presentation">
-  </div>
-</article>
+  <article>
+    <div>
+      <h2 class="h3">Save Time with Prebuilt Workspaces</h2>
+      <p class="text-large">
+        Gitpod continuously builds your git branches like a CI server. This
+        means no more waiting for dependencies to be downloaded and builds to
+        finish.
+      </p>
+      <p>
+        <a href="/docs/prebuilds/" class="btn-secondary"
+          >More about Prebuilt Workspaces</a
+        >
+      </p>
+    </div>
+    <div>
+      <!-- TODO: Insert the terminal output animation from the homepage -->
+      <img src="//placekitten.com/400/400" alt="" role="presentation" />
+    </div>
+  </article>
 
-<article>
-  <div>
-    <img src="//placekitten.com/400/400" alt="" role="presentation">
-    <!-- TODO: Insert "Preparing workspace" images -->
-  </div>
-  <div>
-    <h2 class="h3">Start Fresh with Ephemeral Dev Environments</h2>
-    <p class="text-large">
-      Dev environment as code assures you are always starting from a clean state
-      and never gets dragged into long-living stateful environments. The end of
-      all works-on-my-machine situations.
-    </p>
-  </div>
+  <article>
+    <div>
+      <img src="//placekitten.com/400/400" alt="" role="presentation" />
+      <!-- TODO: Insert "Preparing workspace" images -->
+    </div>
+    <div>
+      <h2 class="h3">Start Fresh with Ephemeral Dev Environments</h2>
+      <p class="text-large">
+        Dev environment as code assures you are always starting from a clean
+        state and never gets dragged into long-living stateful environments. The
+        end of all works-on-my-machine situations.
+      </p>
+    </div>
   </article>
 </section>
 
 <section class="cards double">
   <div class="card">
-    <img src="//placekitten.com/350/150" alt="" role="presentation">
+    <img src="//placekitten.com/350/150" alt="" role="presentation" />
     <!-- TODO: Insert emojis -->
     <h2 class="h4">Collaboration with Workspace Sharing</h2>
     <p>
@@ -112,7 +117,7 @@ section h3:first-child + p {
   <div class="card">
     <!-- TODO: Insert terminal image -->
 
-    <img src="//placekitten.com/350/150" alt="" role="presentation">
+    <img src="//placekitten.com/350/150" alt="" role="presentation" />
     <h2 class="h4">Full linux machine <span>(incl sudo/docker)</span></h2>
     <p>
       Instantly start a container in the cloud based on your Docker image.
@@ -124,7 +129,7 @@ section h3:first-child + p {
 <section class="cards triple">
   <div class="card">
     <!-- TODO: Insert icon -->
-    <img src="//placekitten.com/250/100" role="presentation"  alt="">
+    <img src="//placekitten.com/250/100" role="presentation" alt="" />
     <h3 class="h5">Customize your workspace</h3>
     <p>
       Tweak your environment to suit your needs. From themes to extensions you
@@ -134,17 +139,18 @@ section h3:first-child + p {
   </div>
   <div class="card">
     <!-- TODO: Insert icon -->
-    <img src="//placekitten.com/250/100" alt="" role="presentation">
+    <img src="//placekitten.com/250/100" alt="" role="presentation" />
     <h3 class="h5">GitLab, GitHub, and Bitbucket Integration</h3>
     <p>
       Start your workflow from your Git hosting platform and let Gitpod beam you
       in a ready-to-code dev environment.
     </p>
-    <!-- TODO: Figure out where to link to: --> <a href="">More about Integrations</a>
+    <!-- TODO: Figure out where to link to: -->
+    <a href="">More about Integrations</a>
   </div>
   <div class="card">
     <!-- TODO: Insert icon -->
-    <img src="//placekitten.com/250/100" role="presentation" alt="">
+    <img src="//placekitten.com/250/100" role="presentation" alt="" />
     <h3 class="h5">Code Reviews Within the IDE</h3>
     <p>
       Open pull requests in Gitpod to run, navigate, and review the code. Reply
@@ -165,6 +171,10 @@ section h3:first-child + p {
 
 <section>
   <h2>Get started with screencasts</h2>
-  <!-- TODO: Insert the 3 most recent screencast previews -->
+  <div>
+    {#each firstThreeScreencasts as screencast, index}
+      <ScreencastPreview {screencast} screencastNumber={index + 1} />
+    {/each}
+  </div>
   <a href="/screencasts" class="btn-conversion">See all screencasts</a>
 </section>
