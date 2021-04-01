8d0066ca6a88d68aa5c84109fd4924679edbc01d
Estelle Weyl <estelle@weyl.org>
2021-03-31 18:52:10 -0700

    gitpod vs github page, and a few minor and 1 major change)

--- src/routes/gitpod-vs-github-codespaces.svelte
+++ src/routes/gitpod-vs-github-codespaces.svelte
@@ -10,14 +10,32 @@
     type: "website",
   }}
 />
-<article class="text-blob">
+<header>
   <h1>Gitpod vs GitHub Codespaces</h1>
   <p>
     Gitpod is the faster, more powerful, open-source platform that integrates
     with your individual stack.
   </p>
+</header>
 
-  <div>
+<div class="halfimages">
+  <article class="card">
+    <div>
+      <img
+        src="images/codespace/spaces-1.png"
+        alt=""
+        role="presentation"
+        width="550"
+        height="443"
+      />
+      <p>
+        <small
+          >Disclaimer: All information, prices, and data were last verified on
+          Sept. 25, 2020. To report any incorrect or outdated information,
+          please contact us.</small
+        >
+      </p>
+    </div>
     <div>
       <h2>Ready in a flash</h2>
       <p>
@@ -29,14 +47,16 @@
         >More about Continuous Dev Environments.</a
       >
     </div>
-    <div>
-      <!-- TODO: Insert Gitpod vs Codespaces performance chart -->
-    </div>
-  </div>
+  </article>
 
-  <div>
+  <article class="card">
     <div>
-      <!-- TODO: Insert Codespaces vs Gitpod RAM chart -->
+      <img
+        src="images/codespace/spaces-2.png"
+        alt=""
+        role="presentation"
+        width="550"
+      />
       <p>
         <small
           >Compared resources for 100 active hours at $9/month. Last verified 25
@@ -52,62 +72,68 @@
         running on shared high-powered cloud servers.
       </p>
     </div>
-  </div>
+  </article>
+</div>
 
-  <div>
-    <!-- TODO: Insert Gitpod vs Codespace feature comparison chart. Discuss approach with Mike. -->
-    <p>
-      <small
-        >Disclaimer: All information, prices, and data were last verified on
-        Sept. 25, 2020. To report any incorrect or outdated information, please
-        contact us.</small
-      >
-    </p>
-  </div>
+<div class="brief">
+  <h2>Ready to feel the difference?</h2>
+  <p>Spin up an example workspace now, just link your git provider</p>
+  <a href="TODO:" class="btn-conversion">Try Now</a>
+  <!-- TODO: Insert "Try now" button. What does it do? Should be the same behaviour as on the homepage -->
+</div>
 
-  <div>
-    <h2>Ready to feel the difference?</h2>
-    <p>Spin up an example workspace now, just link your git provider</p>
-    <!-- TODO: Insert "Try now" button. What does it do? Should be the same behaviour as on the homepage -->
-  </div>
-
-  <div>
-    <h2>Open-Source</h2>
-    <p>
-      Gitpod puts developers' interest first and thereby guarantees full
-      transparency and flexibility. Integrate, don't dictate.
-    </p>
-    <ul>
-      <li>
-        Host Gitpod yourself on GCP, AWS, Azure or self-managed Kubernetes
-      </li>
-      <li>Contribute to Gitpod and build customizations yourself</li>
-      <li>Works just as smoothly with GitHub, GitLab and Bitbucket</li>
-    </ul>
-    <!-- TODO: Insert tech icons -->
-  </div>
-
-  <div>
+<div class="halfimages">
+  <article class="card">
     <div>
-      <h3>thefuture.yml</h3>
-      <p>
-        Simply add your build command into a .gitpod.yml file and let Gitpod do
-        the heavy-lifting. Once you’ve experienced the freedom of ephemeral
-        workspaces, you’ll never want to go back to long-lived,
-        manually-maintained environments.
-      </p>
-      <!-- TODO: Insert "Try now" button. What does it do? Should be the same behaviour as on the homepage -->
+      <img
+        src="images/codespace/spaces-3.png"
+        alt=""
+        role="presentation"
+        width="550"
+      />
     </div>
     <div>
-      <h3>Code in a carbon-neutral workspace</h3>
+      <h2>Open-Source</h2>
       <p>
-        Not only is Gitpod more resource-efficient but it also runs on 100%
-        carbon-neutral cloud servers (GCP). All the power, with a lower cost to
-        our planet.
+        Gitpod puts developers' interest first and thereby guarantees full
+        transparency and flexibility. Integrate, don't dictate.
       </p>
-      <a href="https://cloud.google.com/sustainability" target="_blank"
-        >More on GCP carbon neutral</a
-      >
+      <ul>
+        <li>
+          Host Gitpod yourself on GCP, AWS, Azure or self-managed Kubernetes
+        </li>
+        <li>Contribute to Gitpod and build customizations yourself</li>
+        <li>Works just as smoothly with GitHub, GitLab and Bitbucket</li>
+      </ul>
+      <!-- TODO: Insert tech icons -->
     </div>
+  </article>
+</div>
+
+<div class="cards double">
+  <div class="card">
+    <h3>thefuture.yml</h3>
+    <p>
+      Simply add your build command into a .gitpod.yml file and let Gitpod do
+      the heavy-lifting. Once you’ve experienced the freedom of ephemeral
+      workspaces, you’ll never want to go back to long-lived,
+      manually-maintained environments.
+    </p>
+    <p>
+      <a href="TODO:" class="btn-primary">Try Now</a>
+    </p>
   </div>
-</article>
+  <div class="card">
+    <h3>Code in a carbon-neutral workspace</h3>
+    <p>
+      Not only is Gitpod more resource-efficient but it also runs on 100%
+      carbon-neutral cloud servers (GCP). All the power, with a lower cost to
+      our planet.
+    </p>
+    <a
+      href="https://cloud.google.com/sustainability"
+      class="btn-green"
+      target="_blank">More on GCP carbon neutral</a
+    >
+  </div>
+</div>
