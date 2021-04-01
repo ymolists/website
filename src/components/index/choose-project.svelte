17b89cf4a1aaa50d93b5aff7f40e4cfb00f4ed80
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2021-04-01 16:45:50 +0500

    add strikethrough animation.

--- src/components/index/choose-project.svelte
+++ src/components/index/choose-project.svelte
@@ -1,42 +1,147 @@
 <script>
+  import { onMount } from "svelte";
   import Section from "../section.svelte";
+
+  const addStrikethroughs = (targets) => {
+    let t = 0;
+    targets.map((target) => {
+      setTimeout(() => {
+        target.classList.add("strikethrough");
+      }, t);
+      t = t + 400;
+    });
+  };
+
+  const removeStrikethroughs = (targets) => {
+    targets.map((target) => {
+      target.classList.remove("strikethrough");
+    });
+  };
+
+  onMount(() => {
+    let options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1,
+    };
+
+    let observer = new IntersectionObserver(beTouching, options);
+
+    const target = document.querySelector("#observer-target");
+
+    observer.observe(target);
+
+    function beTouching(entries, ob) {
+      entries.forEach((entry) => {
+        const target = entry.target;
+
+        const desktopTargets = Array.from(target.children[1].children);
+        const mobileTargets = Array.from(target.children[2].children);
+        if (entry.isIntersecting) {
+          addStrikethroughs(desktopTargets);
+          addStrikethroughs(mobileTargets);
+        } else {
+          removeStrikethroughs(desktopTargets);
+          removeStrikethroughs(mobileTargets);
+        }
+      });
+    }
+  });
 </script>
 
 <style lang="scss">
   .row {
-    width: 95%;
+    width: 100%;
   }
 
-  h1 {
+  h2 {
     text-align: center;
+    max-width: 67.7rem;
+    margin: 0 auto;
 
     @media (max-width: 1140px) {
-      max-width: 800px;
-      margin: 0 auto;
+      max-width: 50rem;
     }
 
     @media (max-width: 850px) {
-      max-width: 500px;
       font-size: var(--h3);
+    }
+
+    @media (max-width: 682px) {
+      max-width: 23.75rem;
+    }
+
+    @media (max-width: 365px) {
+      font-size: 1.7rem;
     }
   }
 
-  strike {
+  del {
+    text-decoration: none;
+  }
+
+  .desktop {
+    display: none;
+  }
+
+  @media (min-width: 683px) {
+    .desktop {
+      display: initial;
+    }
+
+    .mobile {
+      display: none;
+    }
+  }
+
+  :global(.strikethrough) {
+    display: inline-block;
+    position: relative;
+    transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
     color: var(--light-grey);
+  }
+
+  :global(.strikethrough::after) {
+    content: "";
+    position: absolute;
+    display: block;
+    width: 100%;
+    height: 3px;
+    margin-top: -0.6em;
+    transform-origin: center left;
+    animation: strikethrough 1s 0.5s cubic-bezier(0.55, 0, 0.1, 1) 1 forwards;
+    transition: transform 0.5s cubic-bezier(0.55, 0, 0.1, 1);
+  }
+
+  @keyframes strikethrough {
+    from {
+      transform: scaleX(0);
+      background: var(--light-grey);
+    }
+    to {
+      transform: scaleX(1);
+      background: var(--light-grey);
+    }
   }
 </style>
 
-<Section>
-  <div class="row">
-    <h1>
+<div class="row">
+  <Section>
+    <h2 class="h1" id="observer-target">
       Choose project,
       <br />
-      <strike>
-        check dependencies, checkout branch, view readme.txt, install tools, run
-        build, run test,
-      </strike>
+      <del class="desktop">
+        <span>check dependencies, checkout branch, view</span>
+        <span>readme.txt, install tools, run build, run test,</span>
+      </del>
+      <del class="mobile">
+        <span>check dependencies,</span>
+        <span>checkout branch, view</span>
+        <span>readme.txt, install tools</span>
+        <span>run build, run test,</span>
+      </del>
       <br />
       start coding.
-    </h1>
-  </div>
-</Section>
+    </h2>
+  </Section>
+</div>
