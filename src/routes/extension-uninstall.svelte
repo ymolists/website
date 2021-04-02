9c0759523301a3189064afd32e9432fe72576bfb
Estelle Weyl <estelle@weyl.org>
2021-04-01 21:04:00 -0700

    extension uninstall form. Pull form css out of contact into it's own file

--- src/routes/extension-uninstall.svelte
+++ src/routes/extension-uninstall.svelte
@@ -15,23 +15,104 @@
   const extensionUrl = extensionUrls[currentBrowser.toLowerCase()];
 </script>
 
-{#if extensionUrl}
-  <a href={extensionUrl} target="_blank">Reinstall Extension</a>
-{/if}
+<style>
+  .letter {
+    display: flex;
+    place-content: center center;
+    padding: 3em;
+    order: -1;
+  }
+  .letter p {
+    padding: 2rem;
+    background-color: var(--white);
+    border: 1px solid var(--divider);
+    max-width: 20rem;
+  }
+</style>
 
-<h1>How Can We Improve?</h1>
-<div>
-  <div>
-    <p>
-      Hi there, Sad to see you leave 😕 To improve and make sure that other
-      developers are happier with Gitpod, we’d love to get your opinion on why
-      you decided to uninstall your browser extension. We will work hard to
-      build a product that (hopefully) convinces you to use Gitpod again at a
-      later point ✌️
-    </p>
-  </div>
-  <div>
-    <h2>Why did you uninstall the browser extension?</h2>
+<header>
+  {#if extensionUrl}
+    <a href={extensionUrl} target="_blank">Reinstall Extension</a>
+  {/if}
+  <h1>How Can We Improve?</h1>
+</header>
+<section class=" halfimages">
+  <article class="card">
+    <div class="letter">
+      <p>
+        Hi there, Sad to see you leave 😕 To improve and make sure that other
+        developers are happier with Gitpod, we’d love to get your opinion on why
+        you decided to uninstall your browser extension. We will work hard to
+        build a product that (hopefully) convinces you to use Gitpod again at a
+        later point ✌️
+      </p>
+    </div>
     <!-- TODO: Implement form and Netlify submission -->
-  </div>
-</div>
+    <form method="POST" name="Extension Deletion">
+      <input type="hidden" name="form-name" value="extension-deletion" />
+      <p class="h5">Why did you uninstall the browser extension?</p>
+      <ul>
+        <li>
+          <fieldset>
+            <legend>Check all that apply:</legend>
+            <ul>
+              <li>
+                <input
+                  type="checkbox"
+                  name="reason"
+                  value="usage"
+                  id="usage"
+                  data-text="I never used it"
+                />
+                <label for="usage">I never used it</label>
+              </li>
+              <li>
+                <input
+                  type="checkbox"
+                  name="reason"
+                  value="configuring"
+                  id="configuring"
+                  data-text="I have problems configuring my projects"
+                />
+                <label for="configuring"
+                  >I have problems configuring my projects</label
+                >
+              </li>
+              <li>
+                <input
+                  type="checkbox"
+                  name="reason"
+                  value="local"
+                  id="local"
+                  data-text="I prefer my local development"
+                />
+                <label for="local">I prefer my local development</label>
+              </li>
+              <li>
+                <input
+                  type="checkbox"
+                  name="reason"
+                  value="expected"
+                  id="expected"
+                  data-text="Gitpod isn’t what I expected"
+                />
+                <label for="expected">Gitpod isn’t what I expected</label>
+              </li>
+            </ul>
+          </fieldset>
+        </li>
+        <li>
+          <label for="otherFeedback">Do you have any other feedback?</label>
+          <textarea
+            aria-label="Do you have any other feedback?"
+            id="otherFeedback"
+            name="otherFeedback"
+          />
+        </li>
+        <li>
+          <button class="btn-conversion" disabled type="submit">Send</button>
+        </li>
+      </ul>
+    </form>
+  </article>
+</section>
