b44778ecc4e672a901e10a55d1ec0bfab45e1888
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2021-04-04 01:19:29 +0500

    update the color of the nav items.

--- src/components/main-nav/nav-item.svelte
+++ src/components/main-nav/nav-item.svelte
@@ -3,6 +3,18 @@
 </script>
 
 <style lang="scss">
+  a {
+    color: var(--dark-grey);
+
+    @media (max-width: 768px) {
+      color: var(--black);
+    }
+  }
+
+  a:hover,
+  a:focus {
+    color: var(--black);
+  }
 </style>
 
 <a {href} on:click sveltekit:prefetch><slot /></a>
