5a60bebace6397952c5620a00b96c407e43e6fda
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2021-04-04 18:22:37 +0500

    add visual element below testimonails so that it implies that one can scroll to right.

--- src/components/carousel.svelte
+++ src/components/carousel.svelte
@@ -1,6 +1,31 @@
 <style>
   div {
     scroll-snap-type: x mandatory;
+    scrollbar-width: thick;
+    scrollbar-color: var(--divider) var(--white);
+  }
+
+  div::-webkit-scrollbar {
+    -webkit-appearance: none;
+  }
+
+  div::-webkit-scrollbar:vertical {
+    width: 0.75rem;
+  }
+
+  div::-webkit-scrollbar:horizontal {
+    width: 0.75rem;
+  }
+
+  div::-webkit-scrollbar-thumb {
+    background-color: var(--divider);
+    border-radius: 0.625rem;
+    border: 2px solidvar(--white);
+  }
+
+  div::-webkit-scrollbar-track {
+    border-radius: 0.625rem;
+    background-color: var(--white);
   }
 </style>
 
