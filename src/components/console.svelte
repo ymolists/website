c780cb54a0198f185ab9b34515ba6d2f50f4666d
Nikolai Sivertsen <ns@nikolaisivertsen.net>
2021-04-06 09:55:28 +0200

    Console animation improvements
    * line wrapping
    * alt text
    * better timing
    * handle window resizing
    * only run when in viewport
    * padding

--- src/components/console.svelte
+++ src/components/console.svelte
@@ -1,165 +1,301 @@
-<script lang="ts" context="module">
-  const font_size = 16;
-  const line_height_relative = 1.8;
-  const line_height = Math.round(line_height_relative * font_size);
-
-  const c_black = "rgba(18, 16, 12, 0.7)";
-  const c_orange = "#fc8800";
-  const c_green = "#57c700";
-
-  const re_prompt = /(\s*)(\S+)(\s+)(\S+)(\s+)(\$)(\s+)(.*)/;
-  const re_subshell = /(\s*)(\S+)(\s*)(>)(.*)/;
-  const re_verb = /(\s*)(installing|blocking|updating|downloading|downloaded|compiling|finished|installed)(\s+)(.*)/i;
-</script>
-
 <script lang="ts">
   import { onMount } from "svelte";
 
   export let source = "";
+  export let alt = "";
 
   let wrapper: HTMLDivElement;
-  let width: number;
-  let height: number;
-  let dpr: number;
   let canvas: HTMLCanvasElement;
-  let c: CanvasRenderingContext2D;
 
-  let lines: string[];
-  let current_line: number = 0;
-  let current_char: number = 0;
+  function debounce(fn, wait, callFirst) {
+    var timeout;
+    return function () {
+      if (!wait) {
+        return fn.apply(this, arguments);
+      }
+      var context = this;
+      var args = arguments;
+      var callNow = callFirst && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(function () {
+        timeout = null;
+        if (!callNow) {
+          return fn.apply(context, args);
+        }
+      }, wait);
 
-  $: {
-    lines = source.split("\n");
-    current_line = 0;
+      if (callNow) {
+        return fn.apply(this, arguments);
+      }
+    };
   }
 
   onMount(() => {
-    dpr = devicePixelRatio > 1 ? 2 : 1;
-    c = canvas.getContext("2d");
-    width = wrapper.clientWidth;
-    height = wrapper.clientHeight;
-    canvas.width = dpr * width;
-    canvas.height = dpr * height;
-    canvas.style.width = width + "px";
-    canvas.style.height = height + "px";
-    c.scale(dpr, dpr);
+    let font_size = 16;
+    let line_height = 22;
 
-    c.font = `${font_size}px SF Mono, monospace`;
-    c.fillStyle = c_black;
+    let margins = {
+      top: 72 + line_height,
+      right: 20,
+      bottom: 20,
+      left: 20,
+    };
 
-    let column_width = c.measureText(" ".repeat(64)).width / 64;
+    let colors = {
+      black: "rgba(18, 16, 12, 0.7)",
+      orange: "#fc8800",
+      green: "#57c700",
+    };
 
-    function draw(now) {
-      requestAnimationFrame(draw);
-      c.clearRect(0, 0, width, height);
+    let c = canvas.getContext("2d");
 
-      let offset_y = 72 + line_height;
+    let width;
+    let height;
+    let char_width;
+    let char_max;
 
-      if (current_line * line_height > height - offset_y) {
-        offset_y = -(current_line * line_height - height);
+    function resize() {
+      width = wrapper.clientWidth;
+      height = wrapper.clientHeight;
+
+      let dpr = devicePixelRatio > 1 ? 2 : 1;
+
+      canvas.width = dpr * width;
+      canvas.height = dpr * height;
+      canvas.style.width = width + "px";
+      canvas.style.height = height + "px";
+
+      c.scale(dpr, dpr);
+
+      c.font = `${font_size}px SF Mono, monospace`;
+      c.fillStyle = colors.black;
+
+      char_width = c.measureText(" ".repeat(64)).width / 64;
+      char_max = Math.floor(
+        (width - margins.left - margins.right) / char_width
+      );
+    }
+
+    resize();
+
+    function parse(source) {
+      let color = colors.black;
+      let lines = [];
+      let line;
+      let offset = 0;
+
+      function new_line() {
+        offset = 0;
+        line = { input: [], output: [] };
+        lines.push(line);
       }
 
-      for (let i = 0; i <= current_line; i++) {
-        let x = 20;
-        let y = i * line_height + offset_y;
+      function parse_chunk(type, chunk) {
+        chunk.replace(
+          /(\u001b\[30m)|(\u001b\[31m)|(\u001b\[32m)|(\u001b\[33m)|(\u001b\[39m)|([^\u001b]+)/g,
+          (_, c_black, c_red, c_green, c_yellow, c_default, str) => {
+            if (c_black) color = colors.black;
+            if (c_red) color = colors.orange;
+            if (c_green) color = colors.green;
+            if (c_yellow) color = colors.orange;
+            if (c_default) color = colors.black;
 
-        let offset_c = 0;
-
-        function draw_run(text: string, color?: string) {
-          if (color) c.save(), (c.fillStyle = color);
-          c.fillText(text, x + column_width * offset_c, y);
-          if (color) c.restore();
-          offset_c += text.length;
-        }
-
-        if (y < 0 || y > height) {
-          continue;
-        }
-
-        const line = lines[i];
-
-        const m_prompt = line.match(re_prompt);
-        const m_subshell = line.match(re_subshell);
-        const m_verb = line.match(re_verb);
-
-        if (m_prompt) {
-          const [, s1, user, s2, dir, s3, prompt, s4, command] = m_prompt;
-          draw_run(s1);
-          draw_run(user, c_green);
-          draw_run(s2);
-          draw_run(dir, c_green);
-          draw_run(s3);
-          draw_run(prompt);
-          draw_run(s4);
-
-          if (i === current_line) {
-            if (current_char < command.length - 1) {
-              current_char++;
-
-              draw_run(command.slice(0, current_char + 1));
-
-              if (current_char === command.length - 1) {
-                if (current_line < lines.length - 1) {
-                  current_line++;
-                  current_char = 0;
+            if (str) {
+              let start = 0;
+              let end = Math.min(str.length, char_max - offset);
+              let text;
+              while ((text = str.slice(start, end))) {
+                line[type].push({ type, color, text });
+                offset += end - start;
+                if (offset >= char_max) {
+                  new_line();
                 }
-              }
-            } else {
-              draw_run(command);
-            }
-          } else {
-            draw_run(command);
-          }
-        } else if (m_subshell) {
-          const [, s1, subshell, s2, prompt, rest] = m_subshell;
-          draw_run(s1);
-          draw_run(subshell, c_green);
-          draw_run(s2);
-          draw_run(prompt);
-          draw_run(rest);
-
-          if (i === current_line) {
-            if (current_line < lines.length - 1) {
-              current_line++;
-            } else {
-              if (now % 1000 < 500) {
-                c.save();
-                c.fillStyle = "#12100c";
-                c.fillRect(
-                  x + column_width * offset_c + 4,
-                  y - line_height + font_size,
-                  column_width,
-                  font_size
-                );
-                c.restore();
+                start = end;
+                end = Math.min(str.length, end + char_max - offset);
               }
             }
           }
-        } else if (m_verb) {
-          const [, s1, verb, s2, rest] = m_verb;
-          draw_run(s1);
-          draw_run(verb, c_orange);
-          draw_run(s2);
-          draw_run(rest);
+        );
+      }
 
-          if (i === current_line) {
-            if (current_line < lines.length - 1) {
-              current_line++;
-            }
-          }
+      for (let str of source.split("\n")) {
+        new_line();
+        if (str.indexOf("$") !== -1 || str.indexOf(">") !== -1) {
+          let [, prompt, text] = str.match(/([^$>]+(?:[$>]))(.*)/);
+          parse_chunk("output", prompt);
+          parse_chunk("input", text);
         } else {
-          draw_run(line);
+          parse_chunk("output", str);
+        }
+      }
 
-          if (i === current_line) {
-            if (current_line < lines.length - 1) {
-              current_line++;
+      function count_characters(runs) {
+        return runs.reduce((acc, cur) => acc + cur.text.length, 0);
+      }
+
+      for (let line of lines) {
+        line.input.characters = count_characters(line.input);
+        line.output.characters = count_characters(line.output);
+      }
+
+      return lines;
+    }
+
+    let state = {
+      tick: -1,
+      line: -1,
+      character: -1,
+      lines: parse(source),
+      triggers: {
+        line: 0,
+        character: -1,
+      },
+    };
+
+    function now(trigger) {
+      return state.tick === trigger;
+    }
+
+    function random(min, max) {
+      return Math.floor(min + (max - min) * Math.random());
+    }
+
+    function update() {
+      state.tick += 1;
+
+      if (now(state.triggers.line)) {
+        if (state.line < state.lines.length - 1) {
+          state.line += 1;
+          state.character = 0;
+
+          if (state.character < state.lines[state.line].input.characters - 1) {
+            state.triggers.character = state.tick + random(1, 1);
+          } else {
+            state.triggers.line = state.tick + random(1, 2);
+          }
+        }
+      }
+
+      if (now(state.triggers.character)) {
+        if (state.character < state.lines[state.line].input.characters - 1) {
+          state.character += 1;
+          state.triggers.character = state.tick + random(1, 1);
+        } else {
+          if (state.line < state.lines.length - 1) {
+            if (state.lines[state.line + 1].output.characters) {
+              state.triggers.line = state.tick + random(10, 20);
+            } else {
+              state.triggers.line = state.tick + random(1, 1);
             }
           }
         }
       }
     }
 
-    requestAnimationFrame(draw);
+    function draw(time) {
+      c.clearRect(0, 0, width, height);
+
+      let scroll_y = 0;
+
+      if ((state.line + 2) * line_height >= height - margins.top) {
+        scroll_y = (state.line + 2) * line_height - height + margins.top;
+      }
+
+      for (let i = 0; i <= state.line; i++) {
+        let offset = 0;
+
+        for (let run of state.lines[i].output) {
+          c.fillStyle = run.color;
+          c.fillText(
+            run.text,
+            margins.left + offset * char_width,
+            margins.top + line_height * i - scroll_y
+          );
+          offset += run.text.length;
+        }
+
+        let prompt_length = offset;
+
+        for (let run of state.lines[i].input) {
+          c.fillStyle = run.color;
+          if (i === state.line) {
+            let run_length = state.character - (offset - prompt_length) + 1;
+            c.fillText(
+              run.text.slice(0, run_length),
+              margins.left + offset * char_width,
+              margins.top + line_height * i - scroll_y
+            );
+            if (time % 1000 < 500) {
+              c.fillRect(
+                margins.left + (offset + run_length) * char_width + 0.5,
+                margins.top + line_height * i - scroll_y - line_height * 0.675,
+                char_width,
+                line_height * 0.8
+              );
+            }
+          } else {
+            c.fillText(
+              run.text,
+              margins.left + offset * char_width,
+              margins.top + line_height * i - scroll_y
+            );
+          }
+          offset += run.text.length;
+        }
+      }
+    }
+
+    let handle_resize = debounce(
+      () => {
+        resize();
+        state = {
+          tick: -1,
+          line: -1,
+          character: -1,
+          lines: parse(source),
+          triggers: {
+            line: 0,
+            character: -1,
+          },
+        };
+      },
+      1000,
+      false
+    );
+
+    window.addEventListener("resize", handle_resize);
+
+    let mounted = true;
+    let intersecting = false;
+
+    const observer = new IntersectionObserver(([entry]) => {
+      intersecting = entry.isIntersecting;
+    });
+
+    observer.observe(wrapper);
+
+    function render(time) {
+      if (!mounted) {
+        return;
+      }
+
+      requestAnimationFrame(render);
+
+      if (!intersecting) {
+        return;
+      }
+
+      update();
+      draw(time);
+    }
+
+    requestAnimationFrame(render);
+
+    return () => {
+      mounted = false;
+      window.removeEventListener("resize", handle_resize);
+      observer.disconnect();
+    };
   });
 </script>
 
@@ -170,7 +306,7 @@
   .aspect::before {
     content: "";
     display: block;
-    padding-bottom: 165%;
+    padding-bottom: 125%;
   }
   @media (min-width: 769px) {
     .aspect::before {
@@ -187,6 +323,7 @@
     background: linear-gradient(256.32deg, #fdfdfd 1.45%, #f8f8f8 89.31%);
     box-shadow: 0px 11px 39px #ffca64, 0px 17px 49px rgba(0, 0, 0, 0.1);
     border-radius: 20px;
+    transform: translate3d(0px, 0px, 0px);
   }
   .titlebar {
     z-index: 1;
@@ -196,6 +333,7 @@
     width: 100%;
     height: 72px;
     background: rgba(249, 249, 249, 0.9);
+    -webkit-backdrop-filter: blur(10px);
     backdrop-filter: blur(10px);
   }
   .titlebar::before {
@@ -209,11 +347,23 @@
     border-radius: 8px;
     background: #e7e7e7;
   }
+  figcaption {
+    clip: rect(0 0 0 0);
+    clip-path: inset(50%);
+    height: 1px;
+    overflow: hidden;
+    position: absolute;
+    white-space: nowrap;
+    width: 1px;
+  }
 </style>
 
 <div class="aspect">
-  <div class="wrapper" bind:this={wrapper}>
+  <figure class="wrapper" bind:this={wrapper}>
     <div class="titlebar" />
     <canvas bind:this={canvas} />
-  </div>
+    {#if alt}
+      <figcaption>{alt}</figcaption>
+    {/if}
+  </figure>
 </div>
