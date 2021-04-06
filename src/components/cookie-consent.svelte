83755ac2b9fa909271f85389b89fd37a0c9c0a47
Mike Nikles <mikenikles@gmail.com>
2021-04-06 13:28:09 +0000

    Add button shadow.

--- src/components/cookie-consent.svelte
+++ src/components/cookie-consent.svelte
@@ -17,7 +17,7 @@
 {#if showCookieConsent}
   <div
     out:fade
-    class="fixed bottom-0 px-4 py-3 flex justify-between items-center w-full bg-orange-700 text-xs sm:text-sm md:text-base"
+    class="fixed bottom-0 px-4 py-3 flex justify-between items-center w-full bg-orange-700 shadow-sm text-xs sm:text-sm md:text-base"
   >
     <p class="w-7/12 md:w-9/12">
       This website uses cookies to enhance the user experience. Read our <a
