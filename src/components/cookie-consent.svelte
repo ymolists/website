3946908aac748863dfd55e06a2b3ccb921524035
Mike Nikles <mikenikles@gmail.com>
2021-04-06 02:17:09 +0000

    Add a cookie consent.

---
+++ src/components/cookie-consent.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { onMount } from "svelte";
+  import { fade } from "svelte/transition";
+
+  $: showCookieConsent = false;
+
+  const acceptCookies = () => {
+    showCookieConsent = false;
+    window.localStorage.setItem("cookie-consent", "true");
+  };
+
+  onMount(() => {
+    showCookieConsent = !window.localStorage.getItem("cookie-consent");
+  });
+</script>
+
+{#if showCookieConsent}
+  <div
+    out:fade
+    class="fixed bottom-0 px-4 py-3 flex justify-between items-center w-full bg-orange-700 text-xs sm:text-sm md:text-base"
+  >
+    <p class="w-7/12 md:w-9/12">
+      This website uses cookies to enhance the user experience. Read our <a
+        href="/privacy">privacy policy</a
+      > for more info.
+    </p>
+    <button
+      on:click={acceptCookies}
+      class="px-3 py-2 bg-off-white rounded-2xl md:px-6">Accept Cookies</button
+    >
+  </div>
+{/if}
