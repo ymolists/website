72c0ba38ada35d68936d88714765cdb63c73f9f5
Estelle Weyl <estelle@weyl.org>
2021-04-07 20:23:11 -0700

    Features page header is no longer unique

--- src/components/features/header.svelte
+++ src/components/features/header.svelte
@@ -1,19 +1,6 @@
-<style lang="scss">
-  header {
-    margin-bottom: 0;
-    p {
-      margin-bottom: var(--xx-large);
-
-      @media (max-width: 972px) {
-        margin-bottom: var(--x-large);
-      }
-    }
-  }
-</style>
-
 <header>
-  <h1 class="h2">Features</h1>
-  <p class="text-large mt-4">
+  <h1>Features</h1>
+  <p>
     Automated workspaces that supercharge your development workflow. Squash the
     time between code-to-commit.
   </p>
