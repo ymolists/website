581beea10200b3c33d7b6228138614fda0c6bb34
Mike Nikles <mikenikles@gmail.com>
2021-04-08 01:55:24 +0000

    Develop a reusable banner component; use it for announcements (top) and cookie consent (bottom).

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -4,9 +4,11 @@
   import LayoutRoot from "../components/layout-root.svelte";
   import Nav from "../components/main-nav/index.svelte";
   import Footer from "../components/footer.svelte";
-  import CookieConsent from "../components/cookie-consent.svelte";
+  import AnnouncementBanner from "../components/banners/announcement.svelte";
+  import CookieConsent from "../components/banners/cookie-consent.svelte";
 </script>
 
+<AnnouncementBanner />
 <LayoutRoot>
   <Nav />
   <LayoutMain>
