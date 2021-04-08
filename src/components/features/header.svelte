d2cdbaa421ca6d6928460eee0b9361c35fd254e2
Jan Keromnes <jan.keromnes@typefox.io>
2021-04-08 17:20:21 +0200

    [Features] Clarify header text

--- src/components/features/header.svelte
+++ src/components/features/header.svelte
@@ -1,7 +1,6 @@
 <header>
   <h1>Features</h1>
   <p>
-    Automated workspaces that supercharge your development workflow. Squash the
-    time between code-to-commit.
+    Automated workspaces that supercharge your development workflow. Squash your code-to-commit times.
   </p>
 </header>
