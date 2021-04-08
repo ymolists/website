efeb1af785f6c4d9f96cb51824642ca2ab03889e
Jan Keromnes <janx@linux.com>
2021-04-08 17:53:57 +0000

    [Terminal] Replace '12mins' with '12 minutes'

--- src/contents/terminal.ts
+++ src/contents/terminal.ts
@@ -105,7 +105,7 @@ export const terminalSource = `\u001b[33mGitpod\u001b[39m \u001b[34m/Workspace\u
 
  exit
  🤙 This task ran as a workspace prebuild
- 🎉 Well done on saving 12mins
+ 🎉 Well done on saving 12 minutes
 
 \u001b[33mGitpod\u001b[39m \u001b[34m/Workspace\u001b[39m $ `;
 
