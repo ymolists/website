fa2c2e34ff8ade0e3d46e5bfc87c1ce6e057db87
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2021-05-31 18:26:26 +0500

    add auto prefixer. (#490)
    * add auto prefixer.
    
    * force npm version 6 to be used while developing.

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -6,10 +6,11 @@ const presetEnv = require("postcss-preset-env")({
     "nesting-rules": true, // Optional, not necessary. Read details about it here: https://tabatkins.github.io/specs/css-nesting/#motivation
   },
 });
+const autoprefixer = require("autoprefixer");
 
 const plugins =
   process.env.NODE_ENV === "production"
-    ? [postcssImport, tailwind, presetEnv, cssnano]
+    ? [postcssImport, tailwind, presetEnv, autoprefixer, cssnano]
     : [postcssImport, tailwind, presetEnv];
 
 module.exports = { plugins };
