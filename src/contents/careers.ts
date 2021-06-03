69af5e3515ccb3a60152745c2f06a5cefb475cd6
Johannes Landgraf <johannes.landgraf@pm.me>
2021-06-03 09:49:37 +0200

    Create Senior Frontend Engineer Position (#459)
    * Create Senior Frontend Engineer Position
    
    * Few minor updates; add JS fundamentals.
    
    * Fix typo
    
    * Update src/contents/careers.ts
    
    Co-authored-by: George Tsiolis <tsiolis.g@gmail.com>
    
    Co-authored-by: Mike Nikles <mikenikles@gmail.com>
    Co-authored-by: George Tsiolis <tsiolis.g@gmail.com>

--- src/contents/careers.ts
+++ src/contents/careers.ts
@@ -116,6 +116,50 @@ export const careers: Career[] = [
     date: "2021-01-26T14:00:00.000Z",
   },
   {
+    title: "Senior Software Engineer (Frontend)",
+    intro: `When it comes to code, we’re looking to work with experienced people who can pick a problem and solve it. For us, it’s both how it works and how it looks.`,
+    paragraphs: `We are a highly motivated, fully-remote team, organized in a flat hierarchy, and led by technical founders. We love open-source, have started and led many popular open-source projects, and contribute to many others. In this fashion, we present at international conferences and collaborate openly around the globe.
+  We celebrate diversity and strive for an equal, inclusive, empathetic and welcoming environment. Bringing diversity to the team is a big plus. If you are part of a marginalised community, and are not sure if you should apply, please get in touch.`,
+    lists: [
+      {
+        title: "As part of the team you will ...",
+        items: [
+          "Directly impact users. Gitpod has little fat to trim and changes directly affect users. We always have our users in mind and focus on making Gitpod work for them, not some abstract potential future requirement.",
+          "Be a 'jack of all trades, master of some'. We don't expect you to have a background in everything we use. However, there is loads of room to grow and see everything that makes the platform tick.",
+          "Solve hard technical problems. On the frontend, we deeply care about performance and quality across browsers and platforms. Gitpod is not your typical Kubernetes web app and provides very unique problems you`d be unlikely to come across elsewhere.",
+          "Co-own the product and work self-directed. We work with a very flat hierarchy, organizing our tasks in iterations with a lot of freedom.",
+          "Work with the community and in the open. Our DNA is deeply rooted in open source and we are building Gitpod in public. We expect the same from you.",
+        ],
+      },
+      {
+        title: "What we need",
+        items: [
+          "You share our values. We are developer-led and aim for a frictionless experience when interacting with our product, our company and our brand: no hurdles, no BS, no unnecessary extra steps. We are open minded, inclusive, transparent and curious. We always remain students of the game, not masters of the game. We have opinions, but are not opinionated. We integrate, don`t dictate.",
+          "You care for our mission. Developers architect our future. Gitpod democratises software development by making it more accessible to all developers.  Ready-to-code, cloud-based development environments streamline how software is written today.",
+          "You have strong JavaScript fundamentals. Besides knowing Array methods and how to format numbers and dates with localization in mind, you also write semantic HTML5. You have in-depth knowledge of a modern web framework (React and/or Svelte preferred, but Vue, Angular, etc. are ok too). You care about or are interested in a11y and performance. You know what happens from the time you hit Enter in a browser's URL bar until you see the website rendered on the screen.",
+          "You care about design. You have experience building and advocating for reusable and accessible components as well as passion for beautiful user interfaces and great user experiences. We use Tailwind CSS and if you are already familiar with it, do let us know.",
+          'You have experience shipping code to production. Your code is ready for prime time, and you don`t expect others to test it thoroughly for you. You understand the difference between "making something work" and "making something work reliably". You have read other people`s code, know how to make yourself at home and understand that code is read way more often than it`s written. You understand that every new dependency comes at a cost, as do lines of code.',
+          "You are product minded. You enjoy using what you`ve helped build, and still pay attention to things that feel off. You can empathize with other users and understand how they might see things differently, or that messaging that`s completely clear to you might be unintelligible to others. You are eager to understand why things are not working for others and quickly act when unexpected things happen.",
+          "You are a good communicator. We work remote, our users are remote too. You value writing and express yourself concisely. You may hold strong opinions, but you are not ideological. You communicate thoughtfully, always assume others speak in good faith, aim to understand opposing perspectives and compromise when needed.",
+        ],
+      },
+      {
+        title: "Our stack:",
+        items: [
+          "Gitpod is a distributed application running on Kubernetes. Services are written in Go and TypeScript (Node.JS), while all things frontend are React and TypeScript. We communicate using gRPC, RabbitMQ and JSON-RPC. We develop Gitpod in Gitpod, build using leeway and werft and automatically deploy preview environments for every branch.",
+          "Browser-based productivity tools for the rest. Gitpod for development, GitHub for issue tracking and as git hoster, Slack for comms, Figma for design, Notion as our virtual HQ, Pitch for slides, Loom for screencasts and a few others.",
+        ],
+      },
+      {
+        title: "We're about balance",
+        items: perks.map((perk) => perk.title),
+      },
+    ],
+    textAfterTheLists: "",
+    rendered: false,
+    date: "2021-01-26T14:00:00.000Z",
+  },
+  {
     title: "Senior Software Engineer (Full-Stack)",
     intro: `We at Gitpod build tools that require a view across the stack.`,
     paragraphs: `We are a highly motivated, fully-remote team, organized in a flat hierarchy, and led by technical founders. We love open-source, have started and lead many popular open-source projects, and contribute to many others. In this fashion, we present at international conferences and collaborate openly around the globe.
