69118f4fd06921674631fd2b5a7b0363f41dcbbf
Mike Nikles <mikenikles@gmail.com>
2021-06-21 01:38:20 +0000

    Do not send browser uninstall feedback to Slack.

--- src/functions/feedback/_browser-extension.ts
+++ src/functions/feedback/_browser-extension.ts
@@ -14,12 +14,8 @@ export const submitFeedback = async (body: string): Promise<Response> => {
     sheetTitle: "Extension - Raw Feedback",
     data: [new Date(), feedback.browser, feedback.feedback, feedback.note],
   });
-  const isSentToSlack = await sendFeedbackToSlack(`Browser extension uninstalled.
-Browser: ${feedback.browser}
-Feedback: ${feedback.feedback}
-Note: ${feedback.note ? feedback.note : "N/A"}`);
 
-  const statusCode = isSavedInSheet && isSentToSlack ? 201 : 500;
+  const statusCode = isSavedInSheet ? 201 : 500;
   return {
     statusCode,
     body: statusCode === 201 ? "Feedback added" : "Oh no, something failed.",
