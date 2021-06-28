0a129a52e5c1a42a5d7f8bb989b7701c1f85da5f
opensas <opensas@gmail.com>
2021-06-28 00:55:17 -0300

    fixes #192 - autoclose faq (#627)
    * fixes #192 - autoclose faq
    
    * refs #192 - move Faqs & Faq to their own folder

--- src/components/faqs/faq.svelte
+++ src/components/faqs/faq.svelte
@@ -1,5 +1,19 @@
 <script>
+  import { faqsKey } from "./faqs.svelte";
+  import { getContext } from "svelte";
+
   export let title;
+
+  const activeFaq = getContext(faqsKey);
+
+  const setActive = ({ target }) => {
+    const open = target.open;
+    if (open) $activeFaq = title;
+    // closing the faq that was active, no faq will remain open
+    if (isActive && !open) $activeFaq = null;
+  };
+
+  $: isActive = $activeFaq === title;
 </script>
 
 <style lang="scss">
@@ -86,7 +100,7 @@
   }
 </style>
 
-<details class="faq">
+<details class="faq" open={isActive} on:toggle={setActive}>
   <summary class="faq__top">
     <h3 class="h4 faq__title inline">{title}</h3>
     <img
