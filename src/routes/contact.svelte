4e443f4e300d5dfe70c7bdbbd28f6247a81f557f
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2021-08-27 14:28:51 +0200

    fix: new line firefox (#894)
    * fix: new line firefox
    
    * apply white-space pre-wrap to textarea elements globally.
    
    Co-authored-by: Cahllagerfeld <Cahllagerfeld>
    Co-authored-by: Nisar Hassan Naqvi <syednisarhassan12@gmail.com>

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -176,7 +176,11 @@
   {/each}
 </div>
 
-<section class="card shadow-xl mb-32 sm:mx-8" bind:this={sectionStart} id="form">
+<section
+  class="card shadow-xl mb-32 sm:mx-8"
+  bind:this={sectionStart}
+  id="form"
+>
   {#if isEmailSent}
     <SubmissionSuccess
       title="Thank you for your message"
