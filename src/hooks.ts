080707213cbf85410fe58008457083ad4997fb61
Mike Nikles <788827+mikenikles@users.noreply.github.com>
2021-09-15 22:40:42 -0700

    Allow gitpod.io subdomains to embed the website. (#971)

--- src/hooks.ts
+++ src/hooks.ts
@@ -63,7 +63,7 @@ export const handle: import("@sveltejs/kit").Handle = async ({
       ...response.headers,
       "Set-Cookie": "gitpod-user=loggedIn; Domain=.gitpod.io; Path=/",
       // Avoid clickjacking attacks, see https://cheatsheetseries.owasp.org/cheatsheets/Clickjacking_Defense_Cheat_Sheet.html
-      "Content-Security-Policy": "frame-ancestors none;",
+      "Content-Security-Policy": "frame-ancestors *.gitpod.io;",
     },
   };
 };
