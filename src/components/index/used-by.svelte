fb191d1e8e20e36b6bd1600312e3282227f3e33e
Philipp Maegel <philipp@gitpod.io>
2021-09-30 07:38:13 +0000

    changed to 400k from 450k devs

--- src/components/index/used-by.svelte
+++ src/components/index/used-by.svelte
@@ -1,10 +1,12 @@
 <script>
-import Section from "../section.svelte";
+  import Section from "../section.svelte";
 
   import Brands from "./brands.svelte";
 </script>
 
-<Section class="text-center bg-off-white rounded-2xl shadow-normal pt-small pb-9">
-  <h2 class="h3">Used by +450k developers</h2>
+<Section
+  class="text-center bg-off-white rounded-2xl shadow-normal pt-small pb-9"
+>
+  <h2 class="h3">Used by 400k+ developers</h2>
   <Brands />
 </Section>
