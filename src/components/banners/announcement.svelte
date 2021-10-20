127658080b4be834d139e3016df5ce9145d696e1
Cahllagerfeld <Cahllagerfeld>
2021-10-20 17:43:02 +0000

    fix: width of close button

--- src/components/banners/announcement.svelte
+++ src/components/banners/announcement.svelte
@@ -22,21 +22,23 @@
         href="https://www.gitpod.io/blog/openvscode-server-launch">Read blog</a
       >
     </p>
-    <button on:click={closeBanner} class="flex-1"
-      ><svg
-        width="16"
-        height="16"
-        viewBox="0 0 16 16"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        ><title>Close</title>
-        <path
-          d="M3.00107 3L13 13M12.9989 3L3 13"
-          stroke="#12100C"
-          stroke-opacity="0.75"
-          stroke-linecap="round"
-        />
-      </svg>
-    </button>
+    <div class="flex-1">
+      <button on:click={closeBanner}
+        ><svg
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          ><title>Close</title>
+          <path
+            d="M3.00107 3L13 13M12.9989 3L3 13"
+            stroke="#12100C"
+            stroke-opacity="0.75"
+            stroke-linecap="round"
+          />
+        </svg>
+      </button>
+    </div>
   </Banner>
 </div>
