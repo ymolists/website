b22c935f4ce97ac5b698a4e2372b4d6e1539586e
nisarhassan12 <syednisarhassan12@gmail.com>
2021-12-05 01:49:43 +0500

    Revert "add the beta tags for the ides that are available in beta."
    This reverts commit e574aa1baa59fd1d657b3f0e9f06d4d62db7bf11.

--- src/components/index/ide-switcher.svelte
+++ src/components/index/ide-switcher.svelte
@@ -90,7 +90,7 @@
 <div
   class="mt-macro md:mt-0 md:absolute md:top-0 md:-right-1 lgx:-right-2 flex space-x-1 sm:space-x-2 md:space-x-0 justify-center md:w-min md:block space-y-0 md:space-y-2"
 >
-  {#each ides as { name, availibility, label, icon, screenshots }}
+  {#each ides as { name, label, icon, screenshots }}
     <button
       class="block relative cursor-pointer"
       on:mouseenter={(e) => {
@@ -106,15 +106,11 @@
       >
         <img src="/svg/index/{icon}" alt={label} class="icon" />
       </div>
-      {#if availibility}
-        <div
-          class="hidden absolute -top-3 md:-top-1 left-1/2 transform -translate-x-1/2 md:transform-none md:-left-10 lg:left-3/4 items-center justify-center h-4 w-10 sm:h-5 sm:w-14 text-xs font-semibold text-gray-900 rounded-md sm:rounded-lg shadow-light"
-          class:bg-pink-900={availibility === "soon"}
-          class:bg-orange-700={availibility === "beta"}
-        >
-          {availibility.charAt(0).toUpperCase() + availibility.slice(1)}
-        </div>
-      {/if}
+      <div
+        class="hidden absolute -top-3 md:-top-1 left-1/2 transform -translate-x-1/2 md:transform-none md:-left-10 lg:left-3/4 items-center justify-center h-4 w-10 sm:h-5 sm:w-14 text-xs font-semibold text-gray-900 bg-pink-900 rounded-md sm:rounded-lg shadow-light"
+      >
+        Soon
+      </div>
     </button>
   {/each}
 </div>
