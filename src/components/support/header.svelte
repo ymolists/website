598e6fc689c33c8a2dcc080bebab009ec34d30be
Mike Nikles <mikenikles@pm.me>
2021-12-19 04:31:35 +0000

    Update the search bar placeholder text.

--- src/components/support/header.svelte
+++ src/components/support/header.svelte
@@ -11,7 +11,7 @@
 <header>
   <h1>Need help?</h1>
   <Search
-    placeholder="Search"
+    placeholder="Search Documentation"
     class="text-left py-7 sm:pl-medium h-14"
     containerClasses="roun max-w-lg mx-auto shadow rounded-xl"
     iconClasses="h-6 w-6 left-4 sm:left-5"
