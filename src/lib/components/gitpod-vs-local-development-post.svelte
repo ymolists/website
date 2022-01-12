a59c5a2409773bd967b7815abe156403f257b04b
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2022-01-12 18:00:58 +0500

    add the missing post card. (#1456)

--- src/lib/components/gitpod-vs-local-development-post.svelte
+++ src/lib/components/gitpod-vs-local-development-post.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
   import Section from "./section.svelte";
+  import PostPreview from "./blog/post-preview.svelte";
 
   export let title: string;
   export let text: string;
+
+  import { session } from "$app/stores";
+  import type { BlogPost } from "$lib/types/blog-post.type";
+  const post = $session.posts.find(
+    (p: BlogPost) =>
+      p.slug === "i-said-goodbye-to-local-development-and-so-can-you"
+  );
 </script>
 
 <Section>
@@ -11,5 +19,8 @@
     <p class="text-large">
       {@html text}
     </p>
+  </div>
+  <div class="max-w-7xl mx-auto mt-small">
+    <PostPreview {post} type="blog" layout="row" isMostRecent={true} />
   </div>
 </Section>
