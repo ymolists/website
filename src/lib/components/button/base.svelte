45f1bb5b5b258f732a4a22173c77df79543d85ef
nisarhassan12 <syednisarhassan12@gmail.com>
2022-01-14 16:20:54 +0500

    wip.

---
+++ src/lib/components/button/base.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import type { Button } from "$lib/types/button.type";
+
+  let clazz = "";
+  export { clazz as class };
+  export let button: Button;
+
+  const { href, text } = button;
+  const target = href ? "_blank" : undefined;
+</script>
+
+<a
+  {href}
+  {target}
+  class="inline-block text-center font-semibold py-macro px-xx-small rounded-xl shadow-light text-btn-small leading-4 text-black {clazz}"
+>
+  {text}
+</a>
---
+++ src/lib/types/button.type.ts
@@ -0,0 +1,5 @@
+export type Button = {
+  as?: "link" | "button";
+  href?: string;
+  text?: string;
+};
