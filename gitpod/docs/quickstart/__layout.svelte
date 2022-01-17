99685daf2c92b1947fae5800a9f8cea240fe143c
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-01-17 14:07:02 +0100

    feat: ignore quickstarts for SEO (#1469)

---
+++ gitpod/docs/quickstart/__layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+</script>
+
+<svelte:head>
+  {#if !$page.url.pathname.endsWith("quickstart")}
+    <meta name="robots" content="noindex" />
+  {/if}
+</svelte:head>
+
+<slot />
