c2202d3dc9c62205744a0cc8b951f69297975fe0
Julian <julian@gitpod.io>
2022-03-02 07:23:03 +0000

    feat: create email-widget

---
+++ src/lib/components/blog/e-mail-widget.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { trackEvent } from "../segment.svelte";
+  import Input from "$lib/components/ui-library/input";
+  import Button from "$lib/components/ui-library/button";
+
+  let email = "";
+  let resultMessage: string;
+  let isSubmittedOnce = false;
+  let clazz = "";
+  let hasError: boolean = false;
+  export { clazz as class };
+  const type = "blog";
+
+  const submitFeedback = async () => {
+    if (email.length == 0) {
+      hasError = true;
+      return;
+    }
+    isSubmittedOnce = true;
+
+    trackEvent("feedback_submitted", {
+      email: email,
+      url: window.location.href,
+      path: window.location.pathname,
+    });
+
+    const response = await fetch("/.netlify/functions/feedback", {
+      method: "post",
+      body: JSON.stringify({
+        type,
+        email,
+        url: $page.url.toString(),
+      }),
+    });
+
+    if (response.status === 201) {
+      resultMessage = "Thanks for your feedback, we appreciate it.";
+    } else {
+      resultMessage = "Oh no, something went wrong :(.";
+    }
+    setTimeout(() => {
+      hasError = false;
+      email = "";
+      resultMessage = "";
+    }, 5000);
+  };
+</script>
+
+<div class={clazz}>
+  <div
+    class="bg-white shadow-normal rounded-2xl max-w-md p-x-small m-auto"
+    data-analytics={`{"dnt":true}`}
+  >
+    {#if resultMessage}
+      <p class="text-center">{resultMessage}</p>
+    {:else}
+      <form on:submit|preventDefault={submitFeedback}>
+        <div class="flex justify-center space-x-6" />
+        <div class="mt-x-small">
+          <Input
+            {hasError}
+            label="Submit Email Address"
+            bind:value={email}
+            name="Email"
+            type="email"
+          />
+          <div>
+            <span>
+              <Button
+                variant="primary"
+                size="medium"
+                disabled={isSubmittedOnce}
+                class="mt-micro"
+                type="submit"><span>Send</span></Button
+              >
+            </span>
+          </div>
+        </div>
+      </form>
+    {/if}
+  </div>
+</div>
