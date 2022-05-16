cb578b86c16bff599fbf61f8c9ef402756e942e4
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2022-05-16 15:56:33 +0500

    quotes component updates. (#2051)
    * quotes component updates.
    
    * change the quote on click.

--- src/lib/components/quotes.svelte
+++ src/lib/components/quotes.svelte
@@ -4,14 +4,41 @@
   export let quotes: Quote[];
   let selectedQuote: Quote = quotes[0];
   import LinkButton from "$lib/components/ui-library/link-button";
+  import { onMount } from "svelte";
 
   let clazz = "";
   export { clazz as class };
+
+  let isHovered = false;
+  let interval: any;
+
+  const swipe = () => {
+    interval = setInterval(() => {
+      if (isHovered) return;
+      let currentIndex = quotes.findIndex(
+        (quote) => quote.text === selectedQuote.text
+      );
+      if (currentIndex === quotes.length - 1) {
+        currentIndex = 0;
+        selectedQuote = quotes[0];
+      } else {
+        selectedQuote = quotes[currentIndex + 1];
+      }
+    }, 3000);
+  };
+
+  onMount(() => {
+    swipe();
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
 </script>
 
 <style lang="postcss">
   div :global(.quotes) {
-    max-width: 1000px !important;
+    max-width: 1182px !important;
   }
 
   .square {
@@ -28,61 +55,74 @@
     size="medium"
     class="quotes pt-micro px-0 sm:px-xx-small sm:pt-x-small sm:pb-micro md:pb-medium lg:px-small mx-auto {clazz}"
   >
-    <div class="flex justify-around flex-wrap max-w-3xl mx-auto">
-      {#each quotes as quote}
-        <button
-          class="flex justify-center py-3 group mx-xx-small sm:mx-micro transition-all duration-200"
-          class:opacity-60={selectedQuote !== quote}
-          class:grayscale={selectedQuote !== quote}
-          on:mouseenter={() => (selectedQuote = quote)}
-          on:click={() => (selectedQuote = quote)}
-        >
-          {#if quote.companyLogo.src}
-            <img
-              src={quote.companyLogo.src}
-              alt={quote.companyLogo.alt}
-              class=""
-            />
-          {:else}
-            <svelte:component
-              this={quote.companyLogo}
-              inActive={selectedQuote !== quote}
-            />
-          {/if}
-        </button>
-      {/each}
-    </div>
-    <div
-      class="items-start justify-center mt-macro md:mt-small text-center md:text-left flex-wrap md:flex h-auto"
-    >
-      <div class="w-full md:w-1/2">
-        <img
-          src={selectedQuote.img.src}
-          alt={selectedQuote.img.alt}
-          class:square={selectedQuote.img.square}
-          class="w-full sm:rounded-3xl mx-auto"
-        />
+    <div class="max-w-4xl mx-auto">
+      <div class="flex justify-around flex-wrap max-w-3xl mx-auto">
+        {#each quotes as quote}
+          <button
+            class="flex justify-center py-3 group mx-xx-small sm:mx-micro transition-all duration-200"
+            class:opacity-60={selectedQuote !== quote}
+            class:grayscale={selectedQuote !== quote}
+            on:mouseenter={(e) => {
+              const target = e.target;
+              // @ts-ignore
+              Array.from(target.parentElement.children).map((button) =>
+                // @ts-ignore
+                button.classList.add("opacity-60", "grayscale")
+              );
+              // @ts-ignore
+              e.target.classList.remove("opacity-60", "grayscale");
+              isHovered = true;
+            }}
+            on:mouseleave={() => (isHovered = false)}
+            on:click={() => (selectedQuote = quote)}
+          >
+            {#if quote.companyLogo.src}
+              <img
+                src={quote.companyLogo.src}
+                alt={quote.companyLogo.alt}
+                class=""
+              />
+            {:else}
+              <svelte:component
+                this={quote.companyLogo}
+                inActive={selectedQuote !== quote}
+              />
+            {/if}
+          </button>
+        {/each}
       </div>
       <div
-        class="text flex w-full md:w-1/2 justify-center flex-col flex-1 px-xx-small py-xx-small md:py-0 sm:pl-x-small lg:pl-small lg:pr-0"
+        class="items-start justify-center mt-macro md:mt-small text-center md:text-left flex-wrap md:flex h-auto"
       >
-        <p class="text-large">
-          &ldquo;{selectedQuote.text}&rdquo;
-        </p>
+        <div class="w-full md:w-1/2">
+          <img
+            src={selectedQuote.img.src}
+            alt={selectedQuote.img.alt}
+            class:square={selectedQuote.img.square}
+            class="w-full sm:rounded-3xl mx-auto"
+          />
+        </div>
+        <div
+          class="text flex w-full md:w-1/2 justify-center flex-col flex-1 px-xx-small py-xx-small md:py-0 sm:pl-x-small lg:pl-small lg:pr-0"
+        >
+          <p class="text-large">
+            &ldquo;{selectedQuote.text}&rdquo;
+          </p>
 
-        <p class="font-bold mt-xx-small">
-          {selectedQuote.author}, {selectedQuote.jobTitle}
-        </p>
+          <p class="font-bold mt-xx-small">
+            {selectedQuote.author}, {selectedQuote.jobTitle}
+          </p>
 
-        {#if selectedQuote.link}
-          <div class="mt-xx-small">
-            <LinkButton
-              href={selectedQuote.link.href}
-              variant="tertiary"
-              size="medium">{selectedQuote.link.text}</LinkButton
-            >
-          </div>
-        {/if}
+          {#if selectedQuote.link}
+            <div class="mt-xx-small">
+              <LinkButton
+                href={selectedQuote.link.href}
+                variant="tertiary"
+                size="medium">{selectedQuote.link.text}</LinkButton
+              >
+            </div>
+          {/if}
+        </div>
       </div>
     </div>
   </Card>
