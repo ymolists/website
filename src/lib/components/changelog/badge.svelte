e838e2f24b8b0ce279051bfa3317795961195e3b
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-05-25 17:30:35 +0200

    feat: new Changelog (#2098)

---
+++ src/lib/components/changelog/badge.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  let clazz = "";
+  export { clazz as class };
+  export let text: string;
+  export let variant: "pink" | "orange" = "orange";
+  const capitalised = text.charAt(0).toUpperCase() + text.slice(1);
+</script>
+
+<style lang="postcss">
+  .pink {
+    @apply bg-salmon;
+  }
+
+  .orange {
+    @apply bg-tertiary;
+  }
+</style>
+
+<span
+  class="inline-block px-3 py-0.5 text-xs font-semibold text-important dark:text-black rounded-lg shadow-light {clazz} {variant}"
+>
+  {capitalised}
+</span>
