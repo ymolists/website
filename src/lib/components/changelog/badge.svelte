e25477173ac4ff39174c46067be9c9d472e24120
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-06-09 17:26:57 +0200

    feat: 06.06 Changelog (#2184)
    Co-authored-by: jldec <jurgen@gitpod.io>
    Co-authored-by: George Tsiolis <tsiolis.g@gmail.com>

--- src/lib/components/changelog/badge.svelte
+++ src/lib/components/changelog/badge.svelte
@@ -6,18 +6,10 @@
   const capitalised = text.charAt(0).toUpperCase() + text.slice(1);
 </script>
 
-<style lang="postcss">
-  .pink {
-    @apply bg-salmon;
-  }
-
-  .orange {
-    @apply bg-tertiary;
-  }
-</style>
-
 <span
-  class="inline-block px-3 py-0.5 text-xs font-semibold text-important dark:text-black rounded-lg shadow-light {clazz} {variant}"
+  class:bg-salmon={variant === "pink"}
+  class:bg-tertiary={variant === "orange"}
+  class="inline-block px-3 py-0.5 text-xs font-semibold text-important dark:text-black rounded-lg shadow-light {clazz}"
 >
   {capitalised}
 </span>
