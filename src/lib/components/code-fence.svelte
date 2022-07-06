a9f8b98211ad1248a8616f241756e1e0d048ed4e
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-07-06 10:43:41 +0200

    style: adjust code fences (#2367)
    * style: adjust style for code fences
    
    * style: adjustments
    
    * style: consistent underline

--- src/lib/components/code-fence.svelte
+++ src/lib/components/code-fence.svelte
@@ -29,21 +29,52 @@
     }, 1000);
   }
 
-  $: title = title ?? mapDisplayLanguage(lang);
+  $: tag = title ?? mapDisplayLanguage(lang);
 </script>
 
 <div class="my-8 overflow-y-auto rounded-xl">
   <div
-    class="sticky bg-card top-0 left-0 z-10 py-1 flex items-center backdrop-blur"
+    class="sticky bg-sand-dark dark:bg-light-black top-0 left-0 z-10 py-1 flex items-center"
   >
-    <span class="ml-4 font-mono text-sm ">{title}</span>
+    {#if title}
+      <span class="ml-4 font-mono text-sm ">{tag}</span>
+    {:else}
+      <div class="ml-4 font-mono text-sm flex items-center">
+        <div class="flex items-center gap-1">
+          <img
+            class="dark:hidden"
+            src="/svg/docs/language.svg"
+            alt="language icon"
+          />
+          <img
+            class="hidden dark:block"
+            src="/svg/docs/language-dark.svg"
+            alt="language icon"
+          />
+          <span class="font-semibold text-important">language:</span>
+        </div>
+        <span class="text-important">{tag}</span>
+      </div>
+    {/if}
     <div class="flex-1" />
     <button
       on:click={copyCode}
       class="mr-4 px-2 py-1 transition-all duration-200 delay-[50] hover:text-important"
-      ><span class={copiedSuccessfully ? "hidden opacity-0" : "opacity-100"}
-        >Copy</span
+      ><div
+        class={copiedSuccessfully
+          ? "hidden opacity-0"
+          : "opacity-100 flex items-center gap-1 underline underline-offset-[0.25em] hover:decoration-transparent transition-all duration-200 delay-[50]"}
       >
+        <img
+          class="dark:hidden"
+          src="/svg/headling-link.svg"
+          alt="copy icon"
+        /><img
+          src="/svg/heading-link-dark.svg"
+          class="hidden dark:block"
+          alt="copy icon"
+        /><span>copy code</span>
+      </div>
       <div
         class="transition-opacity z-10 duration-300 rounded-md ease-out
         {copiedSuccessfully ? 'opacity-100' : 'hidden opacity-0'}"
