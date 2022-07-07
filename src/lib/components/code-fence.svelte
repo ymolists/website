66b48cacb14da612e02d10f01956e26537290dfe
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-07-07 12:01:55 +0200

    style: adjust Code Fence (#2374)

--- src/lib/components/code-fence.svelte
+++ src/lib/components/code-fence.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import HeadingLinkIcon from "./svgs/heading-link.svelte";
   export let lang: string = "";
   export let code: string | null = null;
   export let title: string | null = null;
@@ -36,44 +37,38 @@
   <div
     class="sticky bg-sand-dark dark:bg-light-black top-0 left-0 z-10 py-1 flex items-center"
   >
-    {#if title}
-      <span class="ml-4 font-mono text-sm ">{tag}</span>
-    {:else}
-      <div class="ml-4 font-mono text-sm flex items-center">
-        <div class="flex items-center gap-1">
-          <img
-            class="dark:hidden"
-            src="/svg/docs/language.svg"
-            alt="language icon"
-          />
-          <img
-            class="hidden dark:block"
-            src="/svg/docs/language-dark.svg"
-            alt="language icon"
-          />
-          <span class="font-semibold text-important">language:</span>
+    {#if tag}
+      {#if title}
+        <span class="ml-4">{tag}</span>
+      {:else}
+        <div class="ml-4 flex items-center">
+          <div class="flex items-center gap-1">
+            <img
+              class="dark:hidden"
+              src="/svg/docs/language.svg"
+              alt="language icon"
+            />
+            <img
+              class="hidden dark:block"
+              src="/svg/docs/language-dark.svg"
+              alt="language icon"
+            />
+            <span class="font-semibold text-important">language:&nbsp;</span>
+          </div>
+          <span class="text-important">{tag}</span>
         </div>
-        <span class="text-important">{tag}</span>
-      </div>
+      {/if}
     {/if}
     <div class="flex-1" />
     <button
       on:click={copyCode}
-      class="mr-4 px-2 py-1 transition-all duration-200 delay-[50] hover:text-important"
+      class="mr-4 group px-2 py-1 transition-all duration-200 delay-[50] text-[12px] dark:stroke-[#999795] dark:hover:stroke-divider-light hover:stroke-black stroke-[#565252]"
       ><div
         class={copiedSuccessfully
           ? "hidden opacity-0"
-          : "opacity-100 flex items-center gap-1 underline underline-offset-[0.25em] hover:decoration-transparent transition-all duration-200 delay-[50]"}
+          : "opacity-100 flex items-center gap-1 underline group-hover:text-black underline-offset-[0.25em] dark:group-hover:text-important group-hover:decoration-transparent transition-all duration-200 delay-[50]"}
       >
-        <img
-          class="dark:hidden"
-          src="/svg/headling-link.svg"
-          alt="copy icon"
-        /><img
-          src="/svg/heading-link-dark.svg"
-          class="hidden dark:block"
-          alt="copy icon"
-        /><span>copy code</span>
+        <HeadingLinkIcon /><span>copy code</span>
       </div>
       <div
         class="transition-opacity z-10 duration-300 rounded-md ease-out
